61f6b42cf59fd536a51362538c5a7a081fed15c3
DaniilSmirn0v <daniil.smirnov.05@mail.ru>
2022-02-16 13:22:39 +0300

    ui + writing application logic

--- SettingViewColorWithSliders/ViewController.swift
+++ SettingViewColorWithSliders/ViewController.swift
@@ -8,12 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var testView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUi()
+        setupValueLabel()
     }
-
-
+    
+    @IBAction func setupSliderValue(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider: setValue(for: redValueLabel)
+        case greenSlider: setValue(for: greenValueLabel)
+        default:
+        setValue(for: blueValueLabel)
+        }
+    }
 }
 
+extension ViewController {
+    private func setupUi() {
+        testView.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+    }
+    
+    private func setupValueLabel() {
+        setColor()
+        
+        setValue(for: redValueLabel, greenValueLabel, blueValueLabel)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
+    private func setColor() {
+        testView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redValueLabel:
+                label.text = string(from: redSlider)
+            case greenValueLabel:
+                label.text = string(from: greenSlider)
+            default:
+                label.text = string(from: blueSlider)
+            }
+        }
+    }
+}
